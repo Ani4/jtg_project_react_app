9214c3d84ba83354ab2cd903fa119c4984e5c13d
Aniket kumar sharma <sharma.ani.4298@gmail.com>
2020-02-08 23:35:05 +0530

    part 2 of main complete

--- src/components/LifeStyleComponent.js
+++ src/components/LifeStyleComponent.js
@@ -2,21 +2,52 @@
 import React from "react";
 function Block(props) {
   return (
-    <div className="wrapper-inner-right-block">
-      <p className="wrapper-inner-right-block-header">{props.header}</p>
-      <div key={props.i} className="wrapper-inner-right-block-inner">
-        <img src={`/assets/images/lifestyle_thmb_img0${props.i}.png`} />
-        <p className="wrapper-inner-right-block-title-inner">{props.titleI}</p>
-        <p className="wrapper-inner-right-block-content-inner">
-          {props.contentI}
-        </p>
-      </div>
-      <div key={props.j} className="wrapper-inner-right-block">
-        <img src={`/assets/images/lifestyle_thmb_img0${props.j}.png`} />
-        <p className="wrapper-inner-right-block-title-inner">{props.titleJ}</p>
-        <p className="wrapper-inner-right-block-content-inner">
-          {props.contentJ}
-        </p>
+    <div className="wrapper-inner-right">
+      <div className="wrapper-inner-right-block ">
+        <p className="wrapper-inner-right-block-header">{props.header1}</p>
+        <div className="wrapper-inner-right-block-main">
+          <div key={props.i} className="wrapper-inner-right-block-main-inner">
+            <img src={`/assets/images/lifestyle_thmb_img0${props.i}.png`} />
+            <p className="wrapper-inner-right-block-main-inner-title">
+              {props.titleI}
+            </p>
+            <p className="wrapper-inner-right-block-main-inner-content">
+              {props.contentI}
+            </p>
+          </div>
+          <div key={props.j} className="wrapper-inner-right-block-main-inner">
+            <img src={`/assets/images/lifestyle_thmb_img0${props.j}.png`} />
+            <p className="wrapper-inner-right-block-main-inner-title">
+              {props.titleJ}
+            </p>
+            <p className="wrapper-inner-right-block-main-inner-content">
+              {props.contentJ}
+            </p>
+          </div>
+        </div>{" "}
+      </div>{" "}
+      <div className="wrapper-inner-right-block ">
+        <p className="wrapper-inner-right-block-header">{props.header2}</p>
+        <div className="wrapper-inner-right-block-main">
+          <div key={props.k} className="wrapper-inner-right-block-main-inner">
+            <img src={`/assets/images/lifestyle_thmb_img0${props.k}.png`} />
+            <p className="wrapper-inner-right-block-main-inner-title">
+              {props.titleI}
+            </p>
+            <p className="wrapper-inner-right-block-main-inner-content">
+              {props.contentI}
+            </p>
+          </div>
+          <div key={props.l} className="wrapper-inner-right-block-main-inner">
+            <img src={`/assets/images/lifestyle_thmb_img0${props.l}.png`} />
+            <p className="wrapper-inner-right-block-main-inner-title">
+              {props.titleJ}
+            </p>
+            <p className="wrapper-inner-right-block-main-inner-content">
+              {props.contentJ}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -39,34 +70,31 @@ export default function LifeStyle(props) {
               LEARN MORE <img src="/assets/images/arrow.png" />
             </button>
           </div>
-          <div className="wrapper-inner-right">
-            <Block
-              header={"LOREM IPSUM"}
-              i={1}
-              titleI={"CONNECT"}
-              contentI={
-                "Sdolordoloremaue of sece ndolemque dolo Edolo aemqueofdol doloremque services"
-              }
-              j={2}
-              titleJ={"EXPERIENCE"}
-              contentJ={
-                "Saremaue, aemauexy netwdolore redolore Holorquey sdoloredol cremque & foremaue connectivity dolore doloremaue"
-              }
-            />
-            <Block
-              header={"LOREM IPSUM DOLOR"}
-              i={3}
-              titleI={"DISCOVER"}
-              contentI={`Sdolordoloremaue of sece ndolemque dolo 
+
+          <Block
+            header1={"LOREM IPSUM"}
+            i={1}
+            titleI={"CONNECT"}
+            contentI={
+              "Sdolo rdolor emaue of sece ndole mque dolo Edolo aemqu eofdol doloremque services"
+            }
+            j={2}
+            titleJ={"EXPERIENCE"}
+            contentJ={
+              "Saremaue, aemauexy netwdo lore redolore Holorquey sdolo redol cremque & foremaue connectivity dolore doloremaue"
+            }
+            header2={"LOREM IPSUM DOLOR"}
+            k={3}
+            titlek={"DISCOVER"}
+            contentk={`Sdolordoloremaue of sece ndolemque dolo 
                 
               Edolo aemqueofdol doloremque services`}
-              j={4}
-              titleJ={"GROW"}
-              contentJ={`Sdolordoloremaue of sece ndolemque dolo 
+            l={4}
+            titlel={"GROW"}
+            contentl={`Sdolordoloremaue of sece ndolemque dolo 
               
               Edolo aemqueofdol doloremque services`}
-            />
-          </div>
+          />
         </div>
       </div>
     </div>
