cbce85cc0dee3086e86f4f243402a622d89545b5
Aniket kumar sharma <sharma.ani.4298@gmail.com>
2020-02-09 01:43:48 +0530

    over with Customer & News Section

--- src/components/CustomerNewsComponent.js
+++ src/components/CustomerNewsComponent.js
@@ -6,26 +6,27 @@ function DisplayCard(props) {
 function Customer() {
   return (
     <div className="customer">
-      <p className="customer-heading">OUR CUSTOMERS</p>
-      <div className="customer-detail">
+      <p className="heading">OUR CUSTOMERS</p>
+      <div className="customer-detail mt">
         Unde omnis iste natus error sit voluptatem accusantium
         doloremauelaudantium, totam rem aperiam, aque Lorem ipsum dolor it amet,
         consectetur adipis ipsa quaeab ilo inventore veritatis etquasi
         architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam July 1,201
         voluptatem quia voluptas sit aspematur aut odit aut fugit.sed quia
-        consequuntur. learmore
+        consequuntur.&nbsp; <span className="green-word">&nbsp;learn more</span>
       </div>
-      <DisplayCard
-        className="customer-card"
-        imgs={[
-          "custmr_logo01.png",
-          "custmr_logo02.png",
-          "custmr_logo03.png",
-          "custmr_logo04.png",
-          "custmr_logo05.png",
-          "custmr_logo06.png"
-        ]}
-      />
+      <div className="customer-card">
+        <DisplayCard
+          imgs={[
+            "custmr_logo01.png",
+            "custmr_logo02.png",
+            "custmr_logo03.png",
+            "custmr_logo04.png",
+            "custmr_logo05.png",
+            "custmr_logo06.png"
+          ]}
+        />
+      </div>
     </div>
   );
 }
@@ -33,29 +34,30 @@ function Customer() {
 function News() {
   return (
     <div className="news">
-      <div className="news-heading">IN THE NEWS</div>
-      <div className="news-articles">
-        <p className="news-articles-title">
+      <div className="heading">IN THE NEWS</div>
+      <div className="news-articles mt">
+        <p className="news-articles-title green-bold">
           Lorem ipsum dolorsit amet, consectetur adipis
         </p>
-        <p className="news-article-date">July 1, 2011</p>
-        <p className="news-article-detail">
-          Gusantiumdoloremauelaudantium, totam rem aperiam, eaque ipsa quae ab
-          ilo inventorveritatis et quasi archi tecto beatae vitaedicta sunt
-          explicabo enim ipsamvoluptatequia voluptas sit sper. continue reading
+        <p className="news-articles-date">July 1, 2011</p>
+        <p className="news-articles-detail">
+          Corem ipsum dolorsit amet, consectetur adipis, totam rem aperiam,
+          eaque ipsa quae ab ilo inventorveritatis et quasi archi tecto beatae
+          vitaedicta sunt explicabo enim ipsamvoluptatequia voluptas sit sper...{" "}
+          <span className="green-word">&nbsp; continue reading</span>
         </p>
       </div>
 
-      <div className="news-articles">
-        <p className="news-articles-title">
+      <div className="news-articles mt">
+        <p className="news-articles-title green-bold">
           Corem ipsum dolorsit amet, consectetur adipis
         </p>
-        <p className="news-article-date">June 30, 2011</p>
-        <p className="news-article-detail">
-          Gusantiumdoloremauelaudantium, totam rem aperiam, aque ipsa quae ab
-          illo inventoreveritatis et quasi architecto beataevitae dicta sunt
-          explicabo enim psam voluptatem quia voluptas sit asper... continue
-          reading
+        <p className="news-articles-date">June 30, 2011</p>
+        <p className="news-articles-detail">
+          Corem ipsum dolorsit amet, consectetur adipis, totam rem aperiam, aque
+          ipsa quae ab illo inventoreveritatis et quasi architecto beataevitae
+          dicta sunt explicabo enim psam voluptatem quia voluptas sit asper...{" "}
+          <span className="green-word">&nbsp; continue reading</span>
         </p>
       </div>
     </div>
